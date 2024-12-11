71509366440e613b50dd9cfa7de0b405fe5460f5
Agnes-Lydia1305 <98098335+Agnes-Lydia1305@users.noreply.github.com>
2024-12-11 18:48:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("User Registration Form")
 # Add a text input with a caption
 name = st.text_input("Enter your name", placeholder="Type your name here")
-email = st.write("Enter your email", placeholder="Type your email here")
-pwd = st.write("Enter your password", placeholder="Type your password here")
+email = st.text_input("Enter your email", placeholder="Type your email here")
+pwd = st.text_input("Enter your password", placeholder="Type your password here")
   
