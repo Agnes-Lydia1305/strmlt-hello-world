a38857d37a713b53f5d4ac7a1a7ccfa23ec6a8ee
Agnes-Lydia1305 <98098335+Agnes-Lydia1305@users.noreply.github.com>
2024-12-11 18:33:39 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+with st.form("User Registration Form"):
+  st.write("Enter your name")
+  st.text_input
+  st.write("Enter your email")
+  st.text_input
+  st.write("Enter your password")
+  st.text_input
+  st.form_register_button("Register")
