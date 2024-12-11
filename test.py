58ad4228dcb5581a4fba7f67945ff74e59170bd0
Agnes-Lydia1305 <98098335+Agnes-Lydia1305@users.noreply.github.com>
2024-12-11 18:46:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ import streamlit as st
 # Set the page title
 st.title("User Registration Form")
 # Add a text input with a caption
-st.write("Enter your name")
-name = st.text_input("Name", placeholder="Type your name here")
+name = st.text_input("Enter your name", placeholder="Type your name here")
   
