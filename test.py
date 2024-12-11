21b5e6c3e879013f09d6321e55a4a27339a2c8ba
Agnes-Lydia1305 <98098335+Agnes-Lydia1305@users.noreply.github.com>
2024-12-11 18:55:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import streamlit as st
 # Set the page title
 st.title("User Registration Form")
 # Add a text input with a caption
-name = st.text_input("Enter your name", placeholder="Type your name here")
-email = st.text_input("Enter your email", placeholder="Type your email here")
-pwd = st.text_input("Enter your password", placeholder="Type your email here", type="password")
+name = st.text_input("Enter your name")
+email = st.text_input("Enter your email")
+pwd = st.text_input("Enter your password", type="password")
 
 # Add a button with the caption "Register"
 st.button("Register")
